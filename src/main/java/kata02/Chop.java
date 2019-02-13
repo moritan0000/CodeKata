0658e31daabb105ec2b411de8c5710d08f2cb7d0
moritan0000 <moritan0000@gmail.com>
2019-02-13 21:45:23 +0900

    Initial commit

---
+++ src/main/java/kata02/Chop.java
@@ -0,0 +1,26 @@
+package kata02;
+
+class Chop {
+
+    int chop(final int x, final int[] array) {
+
+        int ini = 0;
+        int fin = array.length - 1;
+
+        if (fin == -1 || x < array[0] || x > array[fin]) {
+            return -1;
+        }
+
+        while (true) {
+            if (ini == fin) {
+                return (x == array[ini]) ? ini : -1;
+            } else if (x == array[ini + ((fin - ini) / 2)]) {
+                return ini + (fin - ini) / 2;
+            } else if (x > array[ini + ((fin - ini) / 2)]) {
+                ini = ini + (fin - ini) / 2 + 1;
+            } else {
+                fin = fin - (fin - ini) / 2 - 1;
+            }
+        }
+    }
+}
---
+++ src/main/test/kata02/ChopTest.java
@@ -0,0 +1,139 @@
+package kata02;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChopTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test01() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(3, new int[]{}));
+    }
+
+    @Test
+    void test02() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(3, new int[]{}));
+    }
+
+    @Test
+    void test03() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(3, new int[]{1}));
+    }
+
+    @Test
+    void test04() {
+        Chop a = new Chop();
+        assertEquals(0, a.chop(1, new int[]{1}));
+    }
+
+    @Test
+    void test05() {
+        Chop a = new Chop();
+        assertEquals(0, a.chop(1, new int[]{1, 3, 5}));
+    }
+
+    @Test
+    void test06() {
+        Chop a = new Chop();
+        assertEquals(1, a.chop(3, new int[]{1, 3, 5}));
+    }
+
+    @Test
+    void test07() {
+        Chop a = new Chop();
+        assertEquals(2, a.chop(5, new int[]{1, 3, 5}));
+    }
+
+    @Test
+    void test08() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(0, new int[]{1, 3, 5}));
+    }
+
+    @Test
+    void test09() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(2, new int[]{1, 3, 5}));
+    }
+
+    @Test
+    void test10() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(4, new int[]{1, 3, 5}));
+    }
+
+    @Test
+    void test11() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(6, new int[]{1, 3, 5}));
+    }
+
+    @Test
+    void test12() {
+        Chop a = new Chop();
+        assertEquals(0, a.chop(1, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test13() {
+        Chop a = new Chop();
+        assertEquals(1, a.chop(3, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test14() {
+        Chop a = new Chop();
+        assertEquals(2, a.chop(5, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test15() {
+        Chop a = new Chop();
+        assertEquals(3, a.chop(7, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test16() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(0, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test17() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(2, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test18() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(4, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test19() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(6, new int[]{1, 3, 5, 7}));
+    }
+
+    @Test
+    void test20() {
+        Chop a = new Chop();
+        assertEquals(-1, a.chop(8, new int[]{1, 3, 5, 7}));
+    }
+
+}
